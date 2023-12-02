1218d69b39877c4d917893084612177b1dbbd137
Nevzat Atalay <nevzatatalay79@gmail.com>
2023-12-02 15:18:38 +0300

    update

---
+++ src/index.js
@@ -0,0 +1,11 @@
+
+
+// index.js
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/header/Header'
+import Footer from './components/footer/Footer'
+
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />, rootElement)

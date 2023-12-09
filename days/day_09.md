04ad070ac93820c7cdedc82d3ed41eebd4d1cf05
Nevzat Atalay <nevzatatalay79@gmail.com>
2023-12-09 18:54:57 +0300

    update

---
+++ days/day_09.md
@@ -0,0 +1,100 @@
+## Exercises
+
+### Exercises: Level 1
+
+1. What is conditional rendering?
+```
+In React, conditional rendering is the process of displaying different content based on certain conditions or states. It allows you to create dynamic user interfaces that can adapt to changes in data and user interactions
+```
+2. How do you implement conditional rendering?
+```
+Conditional rendering in React works the same way conditions work in JavaScript. Use JavaScript operators like if or the conditional operator to create elements representing the current state, and let React update the UI to match them.
+```
+3. Which method of conditional rendering do you prefer to use?
+```
+I prefer the ternary operator
+```
+
+### Exercises: Level 2
+
+1. Make a single page application which changes the body of the background based on the season of the year(Autumn, Winter, Spring, Summer)
+```js
+// style.css
+.app{
+    position: relative;
+}
+
+button{
+    padding: 10px 100px;
+    background-color: brown;
+    position: absolute;
+    bottom: 10px;
+    left: 50%;
+    color: #fff;
+    border-radius: 10px;
+}
+button:hover{
+    background-color: yellow;
+    color: black;
+}
+//index.js
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./style.css";
+import winter from "../src/image/winter.jpg"
+import summer from "../src/image/summer.jpg"
+import autumn from "../src/image/autumn.jpg"
+import spring from "../src/image/spring.jpg"
+import season from "../src/image/season.jpg"
+
+
+const seasonArr = [autumn,summer,spring,winter]
+
+const Button = ({onclick}) => (<button onClick={onclick}> Change Season</button>
+)
+
+class App extends React.Component {
+
+  state={
+   styles:{
+    backgroundImage:`url(${season})`,
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    height: "100vh",
+   },
+   index:-1,
+  }
+  
+    changeSeason=()=>{
+      let newindex= this.state.index+1 
+      
+      this.setState({
+        styles:{
+          backgroundImage:`url(${seasonArr[newindex]})`,
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          height: "100vh",
+        },
+        index:this.state.index+1
+      })
+    }
+
+  render(){
+    return(
+      <div className="app" style={this.state.styles}>
+           <Button onclick={this.changeSeason}/>
+      </div>
+    )
+  }
+
+}
+const domNode = document.getElementById("root");
+const root = createRoot(domNode);
+root.render(<App />);
+```
+<img src="../src/image/day9_level2_1.png">
+2. Make a single page application which change the body of the background based on the time of the day(Morning, Noon, Evening, Night)
+
+### Exercises: Level 3
+
+1. Fetching data takes some amount of time. A user has to wait until the data get loaded. Implement a loading functionality of a data is not fetched yet. You can simulate the delay using setTimeout.
--- src/index.js
+++ src/index.js
@@ -1,228 +1,54 @@
-// index.js
-import React from 'react'
-import {createRoot} from 'react-dom/client'
-import nevzat from './image/nevzat.jpg'
-import './style.css'
-// Fuction to show month date year
+
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./style.css";
+import winter from "../src/image/winter.jpg"
+import summer from "../src/image/summer.jpg"
+import autumn from "../src/image/autumn.jpg"
+import spring from "../src/image/spring.jpg"
+import season from "../src/image/season.jpg"
 
 
+const seasonArr = [autumn,summer,spring,winter]
 
-// User Card Component
-const UserCard = ({ user: { firstName, lastName, image } }) => (
-  <div className='user-card'>
-    <img src={image} alt={firstName} />
-    <h2>
-      {firstName}
-      {lastName}
-    </h2>
-  </div>
+const Button = ({onclick}) => (<button onClick={onclick}> Change Season</button>
 )
-
-// A button component
-const Button = ({ text, onClick, style }) => (
-  <button style={style} onClick={onClick}>
-    {text}
-  </button>
-)
-
-// CSS styles in JavaScript Object
-const buttonStyles = {
-  backgroundColor: '#61dbfb',
-  padding: 10,
-  border: 'none',
-  borderRadius: 5,
-  margin: 3,
-  cursor: 'pointer',
-  fontSize: 18,
-  color: 'white',
-}
-
-// class based component
-class Header extends React.Component {
-
-  render() {
-    console.log(this.props.data)
-    const {
-      welcome,
-      title,
-      subtitle,
-      author: { firstName, lastName },
-      date,
-    } = this.props.data
-
-    return (
-      <header style={this.props.styles}>
-        <div className='header-wrapper'>
-          <h1>{welcome}</h1>
-          <h2>{title}</h2>
-          <h3>{subtitle}</h3>
-          <p>
-            {firstName} {lastName}
-          </p>
-          <small>{date}</small>
-        </div>
-      </header>
-    )
-  }
-}
-
-const Count = ({ count, addOne, minusOne }) => (
-  <div>
-    <h1>{count} </h1>
-    <div>
-      <Button text='+1' onClick={addOne} style={buttonStyles} />
-      <Button text='-1' onClick={minusOne} style={buttonStyles} />
-    </div>
-  </div>
-)
-
-// TechList Component
-// class base component
-class TechList extends React.Component {
-
-  render() {
-    const { techs } = this.props
-    const techsFormatted = techs.map((tech) => <li key={tech}>{tech}</li>)
-    return techsFormatted
-  }
-}
-
-// Main Component
-// Class Component
-class Main extends React.Component {
- 
-  render() {
-    const {
-      techs,
-      user,
-      greetPeople,
-      handleTime,
-      changeBackground,
-      count,
-      addOne,
-      minusOne,
-    } = this.props
-    return (
-      <main>
-        <div className='main-wrapper'>
-          <p>Prerequisite to get started react.js:</p>
-          <ul>
-            <TechList techs={techs} />
-          </ul>
-          <UserCard user={user} />
-          <Button
-            text='Greet People'
-            onClick={greetPeople}
-            style={buttonStyles}
-          />
-          <Button text='Show Time' onClick={handleTime} style={buttonStyles} />
-          <Button
-            text='Change Background'
-            onClick={changeBackground}
-            style={buttonStyles}
-          />
-          <Count count={count} addOne={addOne} minusOne={minusOne} />
-        </div>
-      </main>
-    )
-  }
-}
-
-// Footer Component
-// Class component
-class Footer extends React.Component {
-
-  render() {
-    return (
-      <footer style={this.props.state}>
-        <div className='footer-wrapper'>
-          <p>Copyright {this.props.date.getFullYear()}</p>
-        </div>
-      </footer>
-    )
-  }
-}
 
 class App extends React.Component {
-  state = {
-    count: 0,
-    styles: {
-      backgroundColor: '',
-      color: '',
-    },
-  }
-  showDate = (time) => {
-    const months = [
-      'January',
-      'February',
-      'March',
-      'April',
-      'May',
-      'June',
-      'July',
-      'August',
-      'September',
-      'October',
-      'November',
-      'December',
-    ]
 
-    const month = months[time.getMonth()].slice(0, 3)
-    const year = time.getFullYear()
-    const date = time.getDate()
-    return ` ${month} ${date}, ${year}`
+  state={
+   styles:{
+    backgroundImage:`url(${season})`,
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    height: "100vh",
+   },
+   index:-1,
   }
-  addOne = () => {
-    this.setState({ count: this.state.count + 1 })
-  }
-
-  // method which subtract one to the state
-  minusOne = () => {
-    this.setState({ count: this.state.count - 1 })
-  }
-  handleTime = () => {
-    alert(this.showDate(new Date()))
-  }
-  greetPeople = () => {
-    alert('Welcome to 30 Days Of React Challenge, 2020')
-  }
-  changeBackground = () => {}
-  render() {
-    const data = {
-      welcome: 'Welcome to 30 Days Of React',
-      title: 'Getting Started React',
-      subtitle: 'JavaScript Library',
-      author: {
-        firstName: 'Asabeneh',
-        lastName: 'Yetayeh',
-      },
-      date: 'Oct 7, 2020',
-    }
-    const techs = ['HTML', 'CSS', 'JavaScript']
   
-    // copying the author from data object to user variable using spread operator
-    const user = { ...data.author, image: nevzat }
+    changeSeason=()=>{
+      let newindex= this.state.index+1 
+      
+      this.setState({
+        styles:{
+          backgroundImage:`url(${seasonArr[newindex]})`,
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          height: "100vh",
+        },
+        index:this.state.index+1
+      })
+    }
 
-    return (
-      <div className='app'>
-    
-        <Header data={data} state={this.state.backgroundColor} />
-        <Main
-          user={user}
-          techs={techs}
-          handleTime={this.handleTime}
-          greetPeople={this.greetPeople}
-          changeBackground={this.changeBackground}
-          addOne={this.addOne}
-          minusOne={this.minusOne}
-          count={this.state.count}
-        />
-        <Footer date={new Date()} state=    {this.state.backgroundColor}/>
+  render(){
+    return(
+      <div className="app" style={this.state.styles}>
+           <Button onclick={this.changeSeason}/>
       </div>
     )
   }
-}
 
-const container = document.getElementById('root')
-const root = createRoot(container)
-root.render(<App/>)
+}
+const domNode = document.getElementById("root");
+const root = createRoot(domNode);
+root.render(<App />);
